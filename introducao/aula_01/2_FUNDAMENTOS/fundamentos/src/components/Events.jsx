6a1cd37a414916f78f9c615ad0b28e2be9a6c117
Douglas <dougurasu104@hotmail.com>
2025-06-25 10:14:35 -0300

    criação do button com evento handleMyEvent

--- introducao/aula_01/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ introducao/aula_01/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,10 +1,21 @@
 const Events = () => {
+        const handleMyEvent = (e) =>{
+            console.log(e);
+            console.log('Ativou o evento!');
+        }
     return(
         <div>
             <div>
-
+                <button onClick={handleMyEvent}>Clique aqui</button>
             </div>
         </div>
     )
 };
 export default Events;
+/*  Não colocamos os parênteses da funçao
+ handleMyEvent'()' pois, se colocarmos ela será 
+ executada junto à execução do código.
+ Queremos que ela execute apenas quando o houver um 'click'.
+ 
+ Outra coisa é que todo evento possui um parâmetro importante chamado 'event'
+ */
