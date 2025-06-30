7b4b92dfff8046c80c048abdb5b1fd4fa00d947b
Douglas <dougurasu104@hotmail.com>
2025-06-30 10:56:49 -0300

    adiocionamos imagem em App.jsx

--- introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -1,10 +1,18 @@
+import './App.css';
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    
+      <div className="App">
+        <h1>Avançando em React</h1>
+        {/* imagem em public */}
+        <div>
+          <img src="/img1.jpg" alt="Paisagem"/>
+        </div>
+      </div>
+    
   )
 }
 
-export default App
+export default App;
