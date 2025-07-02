159e614a564b74f8f05975cca3703668f216c2b6
Douglas <dougurasu104@hotmail.com>
2025-07-02 10:00:48 -0300

    Criamos o componente .jsx ListRender e aprendemos a usar o .map()

---
+++ introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
@@ -0,0 +1,21 @@
+import { use, useState } from "react"
+
+      const ListRender = () => {
+        const [list, setList] = useState(["Matheus", "Pedro", "Josias"]);
+      return (
+        <div>
+            <ul>
+             {list.map((item) => (
+                <li>{item}</li>
+             )) } 
+            </ul>
+        </div>
+      )
+    }
+    
+    export default ListRender
+
+/*Geralemnte utilizamos '{}' no .map(). Ex.: .map((item) => {...})
+Mas o jsx é considerado um objeto, por isso usamos os '()' na arraow function.alt
+Ex.: .map((item) => (...))
+*/
