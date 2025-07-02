f5422f79d9d70e2afa89c1aecc7afcec79f35fd6
Douglas <dougurasu104@hotmail.com>
2025-07-02 09:43:48 -0300

    Acrescentamos o hook de useState para fins didático

--- introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/components/ManageData.jsx
+++ introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/components/ManageData.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const ManageData = () => {
+        let someData = 10
+        const [number, setNumber] = useState(0);
 
   return (
-    <div>ManageData</div>
+    <div>
+        <div>
+           <p>Valor: {someData}</p> 
+           <button onClick={() => someData = 15}>Mudar variável</button>
+        </div>
+        <div>
+           <p>Valor: {number}</p> 
+           <button onClick={() => setNumber(25)}>Mudar variável</button>
+        </div>
+    </div>
   )
 
 }
