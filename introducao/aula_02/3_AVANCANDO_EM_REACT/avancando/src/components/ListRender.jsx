53701f879c12f67617212307e0ac57587f9a01e8
Douglas <dougurasu104@hotmail.com>
2025-07-03 11:58:10 -0300

    Alterações no listRender explicando a propriedade key

--- introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
+++ introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
@@ -3,18 +3,31 @@ import { use, useState } from "react"
       const ListRender = () => {
         const [list, setList] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
       
+        const [users] = useState([
+          {id: 1, name: "Douglas", age: 31},
+          {id: 72378423, name: "João", age: 28},
+          {id: 8328823, name: "Pedro", age: 44},
+        ])
+
         return (
         <div>
             <ul>
-             {list.map((item) => (
-                <li>{item}</li>
+             {list.map((item, i) => (
+                <li key={i}>{item}</li>
              )) } 
+            </ul>
+            <ul>{/*tente usar sempre esse modelo */}
+              {users.map((user) =>(
+                <li key={user.id}>
+                  {user.name} - {user.age}
+                </li>
+              ))}
             </ul>
         </div>
       )
     }
     
-    export default ListRender
+    export default ListRender;
 
 /*Geralemnte utilizamos '{}' no .map(). Ex.: .map((item) => {...})
 Mas o jsx é considerado um objeto, por isso usamos os '()' na arraow function.alt
