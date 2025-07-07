679af16e318958c010ef59f6940492fca8de7600
Douglas <dougurasu104@hotmail.com>
2025-07-07 11:24:02 -0300

    Criamos o compoenente .jsx ConditionalRender e o importamos em app.jsx

---
+++ introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.jsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+
+const ConditionalRender = () =>{
+    const [x] = useState(true)
+    return(
+        <div>
+            conditionalRender
+        </div>
+    )
+};
+
+export default ConditionalRender;
