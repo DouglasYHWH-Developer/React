c62c065a84ab2e4ed9a04a901373a842b937fa8b
Douglas <dougurasu104@hotmail.com>
2025-07-07 14:03:37 -0300

    Adição do aqruivo .jsx ShowUserName e importamos ele em app.jsx

--- introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ introducao/aula_02/3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import City from "./assets/city.jpg"
 import ConditionalRender from "./components/ConditionalRender";
 import ListRender from "./components/ListRender";
 import ManageData from "./components/ManageData";
+import ShowUserName from "./components/ShowUserName";
 
 
 function App() {
-
+const name = "Joaquim";
+const [userName] = useState("Maria")
   return (
     
       <div className="App">
@@ -23,6 +26,7 @@ function App() {
         <ManageData />
         <ListRender />
         <ConditionalRender />
+        <ShowUserName name={userName} />
       </div>
     
   )
