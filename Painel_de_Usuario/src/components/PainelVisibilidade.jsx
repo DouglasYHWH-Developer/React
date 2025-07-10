90b2f5f39c69f347b3ad31ad58562257cd62f7fd
Douglas <dougurasu104@hotmail.com>
2025-07-10 11:06:09 -0300

    Exercicio concluido

--- Painel_de_Usuario/src/components/PainelVisibilidade.jsx
+++ Painel_de_Usuario/src/components/PainelVisibilidade.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PainelVisibilidade = () => {
+
+    const[mensagem, setMensagem] = useState(false)
+    const alterarMensagem = () => {
+        setMensagem(!mensagem)
+    }
   return (
-    <div>PainelVisibilidade</div>
+    <div>
+        <h3>{mensagem}</h3>
+        <button onClick={alterarMensagem}>
+            {mensagem ? "Ocultar conteúdo" : "Mostrar conteúdo"}
+        </button>
+        {mensagem && <p>Parabéns! A frase apareceu</p>}
+    </div>
+
   )
 }
 
-export default PainelVisibilidade
+export default PainelVisibilidade;
