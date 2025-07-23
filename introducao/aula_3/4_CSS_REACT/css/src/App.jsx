f0166e3baa9d9e37e6cb7c332b82930e24597c5a
Douglas <dougurasu104@hotmail.com>
2025-07-23 14:59:53 -0300

    Aprendemos usar if ternáfio com css inline

--- introducao/aula_3/4_CSS_REACT/css/src/App.jsx
+++ introducao/aula_3/4_CSS_REACT/css/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
+  const n = 15;
+  const [name] = useState("Douglas")
   
   return (
     <>
@@ -14,6 +16,25 @@ function App() {
         <p>Este parágro é do App.jsx</p>
         {/*INLINE STYLE*/}
         <p style={{color: "blue", padding: "25px", borderTop: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
+        {/*INLINE STYLE DINAMICO*/}
+        <h2 style={
+          n < 10 ? 
+          ({color: "purple"}) :
+          ({color: "pink"})}>
+            CSS dinâmico
+          </h2>
+        <h2 style={
+          n > 10 ? 
+          ({color: "purple"}) : 
+          ({color: "pink"})}>
+            CSS dinâmico
+        </h2>
+        <h2 style={
+          name === "Douglas" 
+          ? ({color: "green", backgroundColor: "#000"}) 
+          : null}>
+            Teste dinâmico
+        </h2>
      </div>
     </>
   )
