826ae32e20f31a92ec9c1db5a66226b45ef50322
Douglas <dougurasu104@hotmail.com>
2025-07-23 14:26:01 -0300

    Criação do compoente MyComponente e entendemos como utilizar o css global e de componente usando classes

--- introducao/aula_3/4_CSS_REACT/css/src/App.jsx
+++ introducao/aula_3/4_CSS_REACT/css/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
   
   return (
     <>
      <div className="App">
+      {/*CSS global */}
         <h1>React com CSS</h1>
+        {/*CSS de componente */}
+        <MyComponent />
+        <p>Este parágro é do App.jsx</p>
      </div>
     </>
   )
