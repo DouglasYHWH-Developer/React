cd398eebc7905583cf7a66590b86f753c9fba4a4
Douglas <dougurasu104@hotmail.com>
2025-07-24 14:08:19 -0300

    criação do desafio 5

--- introducao/aula_3/4_CSS_REACT/css/src/components/Title.jsx
+++ introducao/aula_3/4_CSS_REACT/css/src/components/Title.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import styles from "./Title.module.css"
 
 const Title = () => {
   return (
-    <div>Title</div>
+    <div>
+        <h1 className={styles.my_title}>
+            Meu título
+        </h1>
+    </div>
   )
 }
 
-export default Title
+export default Title;
+
+{/*
+<h1 className={styles['my-title']}> o traço não é muito 
+bem visto no React então evite eusar este modelo "className={styles['my-title']}" 
+*/}
