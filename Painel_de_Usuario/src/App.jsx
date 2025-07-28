0bb08d3ebc1dba22d39cce5e835ef358c1d33626
Douglas Miranda <dougurasu104@hotmail.com>
2025-07-28 20:21:23 -0300

    baixando projeto in home

--- Painel_de_Usuario/src/App.jsx
+++ Painel_de_Usuario/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <>
-     { /*<Painel />*/}
-     {/*<PainelVisibilidade />*/}
+     <Painel />
+     <PainelVisibilidade />
      <ListaDeCompras />
     </>
   )
--- Painel_de_Usuario/src/components/ListaDeCompras.jsx
+++ Painel_de_Usuario/src/components/ListaDeCompras.jsx
@@ -2,28 +2,27 @@ import React, { useState } from 'react'
 
 const ListaDeCompras = () => {
 
-    const [listaDeCompras, setListaDeCompras] = useState(["Arroz", "Feijão", "Macarrão"])
-    
-    const removeItens = () =>{
-        setListaDeCompras((item) => {
-            return item.slice(0, -1)
-        })
-    }
+  const [listaDeCompras, setListaDeCompras] = useState(["Arroz", "Feijão", "Macarrão"])
+  const removeItem = () => {
+    setListaDeCompras((item) => {
+      return item.slice(0, -1)
+    })
+  } 
 
-  return (
+  return(
     <div>
-        <ul>
-          {listaDeCompras.map((item)=>(
-            <li>{item}</li>
-          ))}
-        </ul>
-        <button onClick={removeItens}>
-            Remove item
-        </button>
-
-        {!listaDeCompras.length && <p>Lista vazia</p> }  
+      <ul>
+        {listaDeCompras.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <button onClick={removeItem}>
+          Remover último item
+      </button>
+      {!listaDeCompras.length && <p>Lista vazia</p>}
     </div>
   )
+
 }
 
-export default ListaDeCompras
+export default ListaDeCompras;
