3b56cff500da813817a5aaa46ae456c7edd1d9d3
Douglas <dougurasu104@hotmail.com>
2025-07-29 13:47:41 -0300

    Adição do texto .txt Envio de formulário

--- introducao/aula_04/05_FORM_EM_REACT/form/src/components/MyForm.jsx
+++ introducao/aula_04/05_FORM_EM_REACT/form/src/components/MyForm.jsx
@@ -10,6 +10,7 @@ const MyForm = () => {
     setName(e.target.value)
   };
  console.log(name)
+ console.log(email)
   return (
     <div>
         {/* 1 - criação de form */}
@@ -26,7 +27,12 @@ const MyForm = () => {
             {/* 2 - Label envolvendo input*/}
             <label>
               <span>E-mail</span>
-              <input type="email" name="email" placeholder="Digite seu E-mail"/>
+              <input 
+                type="email" 
+                name="email" 
+                placeholder="Digite seu E-mail" 
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </label>
             <input type="submit" value="Enviar"/>
         </form>
