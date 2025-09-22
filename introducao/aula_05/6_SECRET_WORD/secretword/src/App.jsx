852495515328a20e4d051aca0405b6cbe0b24b08
Douglas <dougurasu104@hotmail.com>
2025-09-22 11:05:01 -0300

    Criamos a className "start", criamos o button e estilizamos o button

--- introducao/aula_05/6_SECRET_WORD/secretword/src/App.jsx
+++ introducao/aula_05/6_SECRET_WORD/secretword/src/App.jsx
@@ -1,16 +1,16 @@
 
 import './App.css'
+import StartScreen from './components/StartScreen'
 
 function App() {
  
   return (
-    <>
+    
       <div className="App">
-       <h2>Secret Word</h2>
-      
+       <StartScreen />
       </div>
-    </>
+    
   )
 }
 
-export default App
+export default App;
--- introducao/aula_05/6_SECRET_WORD/secretword/src/components/startScreen.js
+++
@@ -1,9 +0,0 @@
-import "./StartScreen.css"
-
-const startScreen = () => {
-  return (
-    <div>startScreen</div>
-  )
-}
-
-export default startScreen
