985550ca93acbbec92011b14c0c34afcf16d90b1
Douglas <dougurasu104@hotmail.com>
2025-11-06 11:04:55 -0300

    Jogo finalzado

--- jogo_da_velha/jogo-da-velha/src/Game.jsx
+++ jogo_da_velha/jogo-da-velha/src/Game.jsx
@@ -2,10 +2,10 @@ import {useState} from "react";
 import Board from "./Board"
 
 export default function Game() {
-  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
    
 
   function handlePlay(nextSquares) {
