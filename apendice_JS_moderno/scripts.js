7bd24543b367796cf6d0ad6afe60579dfb578d1b
DouglasYHWH-Developer <dougurasu104@hotmail.com>
2025-11-17 18:59:33 -0300

    aprendemos a criar classes

--- apendice_JS_moderno/scripts.js
+++ apendice_JS_moderno/scripts.js
@@ -184,5 +184,24 @@ const produtosDetalhes = {
 const{name1: produtoNome, price, category: produtoCategoria, color} = produtosDetalhes;
 console.log(`${produtoNome}, ${price}, ${produtoCategoria}, ${color}`)
 
-/**********************************Aula 7******************************************* */
+/**********************************Aula 8******************************************* */
 // Classe
+
+class Product {
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+    productWithDiscount(discount){
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product("Camisa gola V", 20); 
+
+console.log(shirt.name)     
+console.log(shirt.productWithDiscount(10))
+console.log(shirt.productWithDiscount(50))
+
+const tenis = new Product("Tênis Nike", 120);
+console.log(tenis.productWithDiscount(20))
